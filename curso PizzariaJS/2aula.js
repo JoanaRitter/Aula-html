f45f95bbd61b4967abb1de72f1e3c44b449d2cec
JoanaRitter <joana.braz.ritter@gmail.com>
2016-06-17 00:10:24 -0300

    Aulas pizzaria JS Eduardo Ritter

---
+++ curso PizzariaJS/2aula.js
@@ -0,0 +1,13 @@
+var pi = 3.14159;
+var r = 100.64;
+var a = pi * Math.pow(r, 2);
+
+console.log(a)
+
+//facilitar auxilio
+
+document.getElementById("numero")     
+
+function calcular() {
+    
+}
